56d5c167d66fa8438e5998d71220985525f24fd8
Vicky～ <vicky@liubingxindeMacBook-Pro.local>
2020-10-03 16:17:48 -0400

    Updates

---
+++ precache-manifest.7b99129a01fd185112881f70cb6dd0e0.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28eb87415b50838986e7701b424c379",
+    "url": "/main/index.html"
+  },
+  {
+    "revision": "20fb556a29c7cc6b976b",
+    "url": "/main/static/css/3.879e9400.chunk.css"
+  },
+  {
+    "revision": "763e8ace1a37d36e6898",
+    "url": "/main/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "f9c116c03aa0f128986c",
+    "url": "/main/static/css/main.0f4312bc.chunk.css"
+  },
+  {
+    "revision": "55401266848454aeca30",
+    "url": "/main/static/js/2.07809275.chunk.js"
+  },
+  {
+    "revision": "71772b085033e92f89cdfb3071333458",
+    "url": "/main/static/js/2.07809275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20fb556a29c7cc6b976b",
+    "url": "/main/static/js/3.02b3eb01.chunk.js"
+  },
+  {
+    "revision": "763e8ace1a37d36e6898",
+    "url": "/main/static/js/4.c50a4cb5.chunk.js"
+  },
+  {
+    "revision": "f9c116c03aa0f128986c",
+    "url": "/main/static/js/main.ecfe2593.chunk.js"
+  },
+  {
+    "revision": "dde8e978bfa3fa25ad27",
+    "url": "/main/static/js/runtime-main.b75ed969.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/main/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/main/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/main/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "b3b9857d46ed202d4a20075be76a283b",
+    "url": "/main/static/media/codeInLogo.b3b9857d.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/main/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/main/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/main/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/main/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/main/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/main/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/main/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/main/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/main/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/main/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
